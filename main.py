b8d356ace0093817b8fcb0ff7c10b0693119c316
Rodrigo Duarte <rodpydev@gmail.com>
2021-12-21 23:32:05 -0500

    ajustes

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import cnpj
+from cnpj import valida, gera, formata
 
 cnpj1 = '04.252.011/0001-10'
 
-if cnpj.valida(cnpj1):
+if valida(cnpj1):
     print(f'{cnpj1} é válido')
 else:
     print(f'{cnpj1} é inválido')
 
 for i in range(100):
-    novo_cnpj = cnpj.gera()
-    formatado = cnpj.formata(novo_cnpj)
+    novo_cnpj = gera()
+    formatado = formata(novo_cnpj)
     print(formatado)
